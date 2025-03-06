3f9c5b7b7dacb48e21f2db93da96f1d58a018a49
Madysen Allard <madysenallard7@gmail.com>
2025-03-05 20:05:53 -0700

    layout for inventoryList and started inventory item to put within list

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,7 @@
+import "./Inventory.scss";
+
+function Inventory() {
+  const [inventoryDetails, setInventoryDetails] = useState([]);
+  const [inventoryInfo, setInventoryInfo] = useState({});
+  // const [isModalOpen, setIsModalOpen] = useState(false)
+}
