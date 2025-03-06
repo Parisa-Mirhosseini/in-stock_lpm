df3a7ab8d8b345ec80c95d2140645fb562a2ef00
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-03-06 11:59:39 -0500

    Front-End: Footer Component (Responsive)

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "../Footer/Footer.scss";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p className="footer__copyright-text">
+                &copy; InStock Inc. All rights Reserved
+            </p>
+        </div>
+    );
+}
+
+
+export default Footer;
