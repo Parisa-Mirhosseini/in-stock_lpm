5e8d9a2a7e5fe5be59003e97518197e48a5ff69f
Madysen Allard <madysenallard7@gmail.com>
2025-03-07 14:21:55 -0700

    functionality and basic styling of the inventory edit page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails.jsx";
 import WarehouseAdd from "./pages/WarehouseAdd/WarehouseAdd.jsx";
 import InventoryDetails from "./pages/InventoryDetails/InventoryDetails.jsx";
 import InventoryAdd from "./pages/InventoryAdd/InventoryAdd.jsx";
+import InventoryEdit from "./pages/InventoryEdit/InventoryEdit.jsx";
 import "./App.scss";
 
 function App() {
@@ -17,12 +18,11 @@ function App() {
         <Route path="" element={<WarehouseList />} />
         <Route path="/warehouses" element={<WarehouseList />} />
         <Route path="/warehouses/add" element={<WarehouseAdd />} />
-        <Route path="/warehouses/:id" element={<WarehouseDetails />} />
         {/* <Route path="/warehouses/edit/:id" element={<WarehouseEdit />} /> */}
         <Route path="/inventories" element={<InventoryList />} />
         <Route path="/inventories/add" element={<InventoryAdd />} />
         <Route path="/inventories/:id" element={<InventoryDetails />} />
-        {/* <Route path="/inventories/edit/:id" element={<InventoryEdit />} /> */}
+        <Route path="/inventories/edit/:id" element={<InventoryEdit />} />
       </Routes>
 
       <Footer />
