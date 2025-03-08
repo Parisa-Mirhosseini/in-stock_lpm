413486b8e49b7e44996524c99815039c7ac508b5
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-03-07 23:46:13 -0500

    Front-End: Warehouse Inventory List

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -2,24 +2,24 @@
 import './WarehouseDetails.scss'
 import { useEffect, useState } from 'react';
 import WarehouseDetailsComponent from '../../components/WarehouseDetailsComponent/WarehouseDetailsComponent';
-// import WarehouseInventoryItems from '../../components/WarehouseInventoryItems/WarehouseInventoryItems';
+import WarehouseInventoryItems from '../../components/WarehouseInventoryItems/WarehouseInventoryItems';
 import { useParams } from 'react-router-dom';
 
-function WarehouseDetails () {
+function WarehouseDetails() {
     const { id } = useParams();
     const [warehousedetails, setWarehouseDetails] = useState([]);
 
-    useEffect(() => {}, [id]);
+    useEffect(() => { }, [id]);
 
 
     return (
         <div className='warehouse-details__wrapper'>
             <section className='warehouse-details__card' >
                 <WarehouseDetailsComponent warehouseId={id} />
-                {/* <WarehouseInventoryItems warehouseId={id} /> */}
+                <WarehouseInventoryItems warehouseId={id} />
             </section>
         </div>
-        
+
     );
 
 }
